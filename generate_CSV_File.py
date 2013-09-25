542d4a98d2f737510131c0c9c33854816bdabe07
M. H. J. Bramwell <mhb.warehouseman@gmail.com>
2013-09-25 16:46:11 -0400

    Use my version of gspread (with OAuth2) to return the calcuated results back into a new sheet in the source Google Spreadsheet.  On branch master  Changes to be committed:
            modified:   generate_CSV_File.py
            new file:   manage_arguments.py

--- generate_CSV_File.py
+++ generate_CSV_File.py
@@ -1,26 +1,171 @@
 #!/usr/bin/python
 #
 
-import argparse, urllib2
-from BeautifulSoup import BeautifulSoup
+import argparse
+import urllib
+
+try :
+
+    from goauth2_helper import GeneratePermissionUrl, AuthorizeTokens, RefreshToken, GenerateOAuth2String
+    
+except ImportError :
+
+    # Get Google oauth2 helper file
+    webFile = urllib.urlopen('http://google-mail-oauth2-tools.googlecode.com/svn/trunk/python/oauth2.py')
+    localFile = open('goauth2_helper.py', 'w')
+    localFile.write(webFile.read())
+    webFile.close()
+    localFile.close()
+    
+    from goauth2_helper import GeneratePermissionUrl, AuthorizeTokens, RefreshToken, GenerateOAuth2String
+
+try :
+    import gspread
+    
+except ImportError :
+
+    github_url = 'https://github.com/martinhbramwell/gspread/tree/master/gspread'
+    print 'You need a sub-directory called "gspread", containing all the files liosted here:\n     %s' % github_url
+    exit(-1)
+
 import getGauth
 import getSpans
 import getBorders
+#import putResult
 
 import os
 import math
 
-def downloadChunks(url, filename):
-    """Helper to download large files
+import urllib2
+
+def pushBackToSpreadsheet(credentials, workbook_key, sheet_name, patchData) :
+
+    print ' --   --   --   --   --   --   --   --   --   --   --   --   --  '
+    print credentials
+    print workbook_key
+    print sheet_name
+
+    # Hook up to Google Spreadsheet stream.
+    conn = gspread.connect(credentials)
+
+    # Get the destination workbook
+    wkbk = conn.open_by_key(workbook_key)
+
+    # Get the target sheet in that workbook 
+    try :
+        wkbk.del_worksheet(wkbk.worksheet(sheet_name))
+    except gspread.WorksheetNotFound as wsnf :
+        pass
+    sht = wkbk.add_worksheet(sheet_name, patchData['h'], patchData['w'])
+
+
+    # Create a lisat of all the target cells
+    rangeSpec = 'A1:{}'.format(sht.get_addr_int(patchData['h'], patchData['w']))
+    allCells = sht.range(rangeSpec)
+
+    w = patchData['w']
+    h = patchData['h']
+
+    # Fill each of the target cells with 
+    cnt = 0
+    for cell in allCells :
+        r = cell.row
+        c = cell.col
+        idx = (r-1) * w + (c-1)
+#        print 'At #{}({}) -- Cell R{}C{} has'.format(idx, cnt, r, c)
+#        print '- -  {}'.format(patchData['data'][r-1][c-1])
+        allCells[idx].value = patchData['data'][r-1][c-1]
+        cnt += 1
+
+    sht.update_cells(allCells)
+    
+    conn.disconnect()
+
+    return
+
+
+def generateTable(dictCells) :
+
+    titleRow = ["sheet", "row", "col", "rows", "cols", "Tstyle", "Tcolor", "Tthick", "Lstyle", "Lcolor", "Lthick", "Bstyle", "Bcolor", "Bthick", "Rstyle", "Rcolor", "Rthick"]
+    
+    no_spans = ["", ""]
+    no_bords = ["", "", "", "", "", "", "", "", "", "", "", ""]
+    
+    table = []
+    table.append(titleRow)
+
+    idx = 2
+    for cell, contents in dictCells.items() :
+#        print 'Doing #{} : Cell = {}, Content = {}'.format(idx, cell, contents)
+        idx += 1
+        row = cell.split(':')
+        
+        if 'span' in contents :
+            row.extend(contents['span'][1:-1].split('", "'))
+        else :
+            row.extend(no_spans)
+        
+        if 'brdr' in contents :
+            row.extend(contents['brdr'][1:-1].split('", "'))
+        else :
+            row.extend(no_bords)
+            
+        table.append(row)
+
+    return {'data': table, 'h' :len(table), 'w' : len(titleRow)}        
+
+
+
+def generateCSV(dictCells) :
+    '''
+    Assemble a CSV string of the data. Rows separated by '\n'
+    
+                This is out of use.  Might not work.
+    '''
+    no_spans = '"", ""'
+    no_bords = '"", "", "", "", "", "", "", "", "", "", "", ""'
+    patchRows = '"sheet", "row", "col", "rows", "cols", "Tstyle", "Tcolor", "Tthick", "Lstyle", "Lcolor", "Lthick", "Bstyle", "Bcolor", "Bthick", "Rstyle", "Rcolor", "Rthick"'
+    
+
+    for cell, contents in dictCells.items() :
+    
+        frag = '"{}", "{}", "{}", '.format(cell.split(':')[0], cell.split(':')[1], cell.split(':')[2])
+        if 'span' in contents :
+            frag += contents['span']
+        else :
+            frag += no_spans
+
+        frag += ', ' 
+
+        if 'brdr' in contents :
+            frag += contents['brdr']
+        else :
+            frag += no_bords
+            
+        patchRows += '\n' + frag
+            
+    return patchRows
+
+
+
+def downloadToFile(url, filename):
+
+    """
+    Helper to download possibly large files
+    
         the only arg is a url
        this file will go to a temp directory
        the file will also be downloaded
        in chunks and print out how much remains
+       
+        use it like this :
+            downloadToFile("http://localhost/a.zip")
+       
     """
 
     baseFile = os.path.basename(filename)
 
-    #move the file to a more uniq path
+    # move the file to a more uniq path
     os.umask(0002)
     temp_path = "/tmp/"
     try:
@@ -46,9 +191,6 @@ def downloadChunks(url, filename):
 
     return open(file)
     
-#use it like this
-#downloadChunks("http://localhost/a.zip")
-
 
 def getConnectionHeader(gauth, google_UID, google_PWD, google_service):
 
@@ -66,82 +208,52 @@ def getConnectionHeader(gauth, google_UID, google_PWD, google_service):
 
 def main():
 
-    desc = 'A tool for getting the row span, column span & / or border style data of one or more sheets in a Google Drive Spreadsheet. (Note that for command line access, Google requires client authentication *even if* a document is "Public to anyone on the web").'
-    usage = "usage: %prog [options] arg"
-    parser = argparse.ArgumentParser(description=desc)
-    
-    parser.add_argument("-t", "--service_type", dest="google_service", default="wise"
-                      , help="If you intend to work with something other than a spreadsheet you'll need to change this. (Default : 'wise')")
-
-    parser.add_argument("-k", "--spreadsheet_key", dest="workbook_key", required=True
-                      , help="The key parameter taken from URL of the spreadsheet. (Required!)")
-                      
-    parser.add_argument("-s", "--sheet_ids", dest="sheet_ids"
-                      , help="A comma separated list of sheet identifiers of the single sheet to be accessed, eg 1,8,4,3. (Default : 0)")
-                      
-    group1 = parser.add_mutually_exclusive_group(required=True)
-    group1.add_argument("-a", "--service_authentication", dest="gauth"
-                      , help="The 'Auth' parameter returned by Google Client Login. (Can't be used with 'user_id')")
-    
-    group1.add_argument("-u", "--user_id", dest="google_UID", help="Thi is the Google user's email address. (Can't be used with 'service_authentication')")
-
-    parser.add_argument("-p", "--user_passwd", dest="google_PWD", help="The Google user's password")
-
-    args = parser.parse_args()
+    from manage_arguments import getArgs
+    a = getArgs()
+    args = a.args
+    credentials = a.credentials
 
     if args.sheet_ids is None:
         return "Found no sheet ID to use."
 
     
-    header = getConnectionHeader(args.gauth, args.google_UID, args.google_PWD, args.google_service)
+    header = getConnectionHeader(args.gauth, args.user_email, args.google_PWD, args.google_service)
+    
 
     url_part  = "https://docs.google.com/feeds/download/spreadsheets/Export?exportFormat=html"
     url_part += "&key=" + args.workbook_key
 
-    patchRows = '"sheet", "row", "col", "rows", "cols", "Tstyle", "Tcolor", "Tthick", "Lstyle", "Lcolor", "Lthick", "Bstyle", "Bcolor", "Bthick", "Rstyle", "Rcolor", "Rthick"'
-    
     dictCells = {}
     for sheet_id in args.sheet_ids.split(','):
 
         request = urllib2.Request(url_part + "&gid=" + str(sheet_id), headers=header)
 
-        html_file = downloadChunks(request, "temporary.html")
+        html_file = downloadToFile(request, "temporary.html")
 
         dictCells = getBorders.getSpreadsheetCellBorders(dictCells, html_file, sheet_id)  #  BORDERS !!!
     
         dictCells = getSpans.getSpreadsheetSpannedCells(dictCells, html_file, sheet_id)  #  SPANS!!!
+        
 
-    no_spans = '"", ""'
-    no_bords = '"", "", "", "", "", "", "", "", "", "", "", ""'
-
-    for cell, contents in dictCells.items() :
+    patchRows = generateTable(dictCells)
     
-        frag = '"{}", "{}", "{}", '.format(cell.split(':')[0], cell.split(':')[1], cell.split(':')[2])
-        if 'span' in contents :
-            frag += contents['span']
-        else :
-            frag += no_spans
+    if args.immediate_load :
 
-        frag += ', ' 
-
-        if 'brdr' in contents :
-            frag += contents['brdr']
-        else :
-            frag += no_bords
-            
-        patchRows += '\n' + frag
-            
+        pushBackToSpreadsheet(credentials, args.workbook_key, args.immediate_load, patchRows)
+        print 'Span and Border specifications have been pushed to the sheet "%s" in the workbook "%s".' % (args.immediate_load, args.workbook_key)
+    
     return patchRows
 
 
 if __name__ == "__main__":
 
     result = main()
-    print result
+#    print result
 	
+'''	
 
-# Example :  ./getSpans.py -k "0AhGdN..S.P.R.E.A.D.S.H.E.E.T...K.E.Y..c1TlE" -s 3 -u "yourUserName@gmail.com" -p "yourPassword" > spans.csv
-#    OR
-# Example :  ./getSpans.py -k "0AhGdN..S.P.R.E.A.D.S.H.E.E.T...K.E.Y..c1TlE" -s 3 -a "yourGoogleAuthKey" > spans.csv
+Example :
 
+./generate_CSV_File.py -ssk 0AhgdNB3-bSxAdDFBQWJ3YTAzd015UFJTZ3FwZlc1TlE -si 2 -ue doowa.diddee@gmail.com -cs 'Zis38NZ_wyBII2Q9xfMRthW-' -ci 204618981389-fod7457tdhtfvmglt287dg7p30579r0l.apps.googleusercontent.com -am ForDevices -ce alicia.factorepo@gmail.com -up 99xxoozz  -il meta_patches meta_patches.csv
 
+'''
---
+++ manage_arguments.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+import argparse
+
+def getArgs() :
+
+    default_sheet_name = 'meta_patches'
+
+#    desc = 'A tool for writing a CSV file into a new/existing sheet in a spreadsheet.\nAn OAuth access token is required.'
+    
+    desc = 'A tool for getting the row span, column span & / or border style data of one or more sheets in a Google Drive Spreadsheet.\n (Note that for command line access, Google requires client authentication *even if* a document is "Public to anyone on the web").\n\nIf OAuth2 arguments are configured and "-i/--immediate_load" is indicated, then the CSV file will be pushed into the same spreadsheet from which the data was first read.'
+    
+    
+    usage = "usage: %prog [options] arg"
+    parser = argparse.ArgumentParser(description=desc)
+
+    # Optional
+    parser.add_argument("-il", "--immediate_load", dest="immediate_load"
+                      , help="The name for the sheet in the target workbook to be created/replaced with the generated CSV. (Default : Null)")
+                      
+    # Required
+    parser.add_argument("-ssk", "--spreadsheet_key", dest="workbook_key", required=True
+                      , help="The key parameter taken from URL of the spreadsheet. (Required.)")
+                      
+    parser.add_argument("-si", "--sheet_ids", dest="sheet_ids", required=True
+                      , help="A comma separated list of sheet identifiers of the single sheet to be accessed, eg 0,1,8,4,3.")
+                      
+    parser.add_argument("-ue", "--user_email", dest="user_email", required=True, help="This is the GMail account of the spreadsheet owner.")
+
+    parser.add_argument("-ci", "--client_id", dest="client_project", required=True, help="This is the Client ID you get from Google's API console.")
+    
+    group1 = parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument("-sa", "--service_authentication", dest="gauth"
+                      , help="The 'Auth' parameter returned by Google Client Login. (Can't be used with 'user_id')")
+    
+#    group1.add_argument("-ui", "--user_id", dest="google_UID", help="Thi is the Google user's email address. )")
+    group1.add_argument("-ce", "--client_email", dest="client_email", required=False, help="This is the GMail account that will be used for SMTP communication with spreadsheet owners. (Can't be used with 'service_authentication'")
+                      
+
+    parser.add_argument("-up", "--user_passwd", dest="google_PWD", help="The Google user's password")
+    
+
+    # Required but default afforded
+    parser.add_argument("-st", "--service_type", dest="google_service", default="wise"
+                      , help="If you intend to work with something other than a spreadsheet you'll need to change this. (Default : 'wise')")
+
+    parser.add_argument("-sn", "--sheet_name", dest="sheet_name", default=default_sheet_name
+                      , help="The name of the sheet to create/replace. (Required. Default : {})".format(default_sheet_name))
+    
+    # Required, but only the first time                  
+    parser.add_argument("-sat", "--smtp_access_token", dest="smtp_access_token", required=False, help="The token you get from Google that allows you to use your own GMail account as an SMTP server, without needing to use your username and password.")
+                      
+    parser.add_argument("-cs", "--client_secret", dest="client_secret", required=False, help="The Client Secret you get from Google's API console.")
+
+    parser.add_argument("-am", "--auth_method", dest="auth_method", required=False, help="Can be either 'ForDevices', 'InstalledApp' or 'ToBeDropped' (to edit storage to drop the connection between project and user)")
+                      
+    parser.add_argument("-srt", "--smtp_refresh_token", dest="smtp_refresh_token", required=False, help="This refreshes the access token that refreshes your authorization to use your GMail account as an SMTP server without having to ask you again.")
+
+    parser.add_argument("-ru", "--args.redirect_uri", dest="redirect_uri", required=False, help="This is the Redirect URI you get from Google's API console.")
+
+
+
+#    parser.add_argument("CSV_file", help="The CSV format data you want to push out to the spreadsheet.(Required. Default : None.)")
+
+    args = parser.parse_args()
+    
+    if args.gauth is None and args.google_PWD is None and args.client_email is not None :
+        print 'You need a password :  -up xxxxxxx'
+        exit(-1)
+    
+       
+    now =       NameSpace({
+                      'debug': False
+                    , 'user_id' : args.user_email
+                    , 'client_id' : args.client_project
+                })
+    user =      {
+                   args.user_email: 
+                   {
+                       args.client_project: 
+                       NameSpace({
+                           'auth_method': args.auth_method
+                       })
+                   }
+                }
+                
+    client =    {
+                    args.client_project:
+                    NameSpace({
+                          'client_secret': args.client_secret
+                        , 'redirect_uri': args.redirect_uri
+                        , 'client_email': args.client_email
+                        , 'smtp_access_token': args.smtp_access_token
+                        , 'smtp_refresh_token': args.smtp_refresh_token
+                    })
+                }
+
+
+    credentials = NameSpace({'gclient': client, 'user' : user, 'now' : now})
+
+    return NameSpace({'args':args, 'credentials': credentials})
+
+class NameSpace(object):
+  def __init__(self, adict):
+    self.__dict__.update(adict)
+
