576bb6cad18683f9686e37e64732e536e194ae62
Hasan <hasan@http_fontus.(none)>
2013-10-20 08:37:20 -0400

    Set more switches to obligatory.  On branch master  Your branch is ahead of 'origin/master' by 2 commits.
     Changes to be committed:
    	modified:   manage_arguments.py

--- manage_arguments.py
+++ manage_arguments.py
@@ -30,18 +30,8 @@ def getArgs() :
 
     parser.add_argument("-ci", "--client_id", dest="client_project", required=True, help="This is the Client ID you get from Google's API console.")
     
-    parser.add_argument("-ce", "--client_email", dest="client_email", required=False, help="This is the GMail account that will be used for SMTP communication with spreadsheet owners.")
+    parser.add_argument("-ce", "--client_email", dest="client_email", required=True, help="This is the GMail account that will be used for SMTP communication with spreadsheet owners.")
     
-    '''
-    group1 = parser.add_mutually_exclusive_group(required=True)
-    group1.add_argument("-sa", "--service_authentication", dest="gauth"
-                      , help="The 'Auth' parameter returned by Google Client Login. (Can't be used with 'user_id')")
-    
-#    group1.add_argument("-ui", "--user_id", dest="google_UID", help="Thi is the Google user's email address. )")
-                      
-
-    parser.add_argument("-up", "--user_passwd", dest="google_PWD", help="The Google user's password")
-    '''
 
     # Required but default afforded
     parser.add_argument("-st", "--service_type", dest="google_service", default="wise"
@@ -53,13 +43,13 @@ def getArgs() :
     # Required, but only the first time                  
     parser.add_argument("-sat", "--smtp_access_token", dest="smtp_access_token", required=False, help="The token you get from Google that allows you to use your own GMail account as an SMTP server, without needing to use your username and password.")
                       
-    parser.add_argument("-cs", "--client_secret", dest="client_secret", required=False, help="The Client Secret you get from Google's API console.")
+    parser.add_argument("-cs", "--client_secret", dest="client_secret", required=True, help="The Client Secret you get from Google's API console.")
 
-    parser.add_argument("-am", "--auth_method", dest="auth_method", required=False, help="Can be either 'ForDevices', 'InstalledApp' or 'ToBeDropped' (to edit storage to drop the connection between project and user)")
+    parser.add_argument("-am", "--auth_method", dest="auth_method", required=True, help="Can be either 'ForDevices', 'InstalledApp' or 'ToBeDropped' (to edit storage to drop the connection between project and user)")
                       
     parser.add_argument("-srt", "--smtp_refresh_token", dest="smtp_refresh_token", required=False, help="This refreshes the access token that refreshes your authorization to use your GMail account as an SMTP server without having to ask you again.")
 
-    parser.add_argument("-ru", "--args.redirect_uri", dest="redirect_uri", required=False, help="This is the Redirect URI you get from Google's API console.")
+    parser.add_argument("-ru", "--args.redirect_uri", dest="redirect_uri", required=True, help="This is the Redirect URI you get from Google's API console.")
 
 
 
